5bb80cf01907b1dfe6b0c7ac0b56afef50ad00f2
AbhishekPipo <Pipo.abhi@gmail.com>
2023-06-27 17:26:01 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'view-child';
+
+  @ViewChild('dobInput') dateOfBirth!: ElementRef<any>;
+  @ViewChild('ageInput')  age!: ElementRef<any>;
+
+  @ViewChild(DemoComponent,{static: true}) demoComp: DemoComponent;
+  constructor() {
+    this.demoComp = new DemoComponent();
+  }
+  
+calculateAge(){
+  let birthYear=new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+  // console.log(this.dateOfBirth);
+  // console.log(this.age);
+
+  let cur=new Date().getFullYear();
+  let age=cur-birthYear;
+  this.age.nativeElement.value=age;
+}
+
 }
